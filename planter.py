1a93c1b7d09f552a8e19b543f3ed63381c1f16aa
Lindsay Barton <lindsaybarton76@gmail.com>
2025-01-19 23:18:38 -0500

    01-19-2025

--- planter.py
+++ planter.py
@@ -1,9 +1,11 @@
 from tkinter import *
 root = Tk()
 
+root.geometry("400x400")
+
 
 drawing_window_frame = Frame(root)
-drawing_window = Canvas(root, width = 400, height = 250)
+drawing_window = Canvas(root, width = 400, height = 400)
 
 
 drawing_window_frame.pack()
@@ -15,11 +17,20 @@ point_coordinates = []
 point_coordinates_window = []
 
 def add_point(event):
+
     x, y = event.x, event.y
+
+    label = Label(drawing_window_frame, text=f"{x} : {y}")
+    label.pack(pady=40)
+
     point_coordinates.append((x, y))
     point_coordinates_window.append((x, y))
     if(len(point_coordinates_window) > 1):
         draw_connected_line()
+        label.destroy()
+
+
+
 
 ##draw the line, then pop the first coordinate so that the last coordinate is the first one now for the next line
 def draw_connected_line():
@@ -34,10 +45,14 @@ def stop(event):
     point_coordinates_window.pop()
     drawing_window.create_line((point_coordinates[0], (0, 0)), fill='black', width=5)
 
+##updates mouse label with distance between two points
+def update_mouse_label():
+    drawing_window.after(500, update_mouse_label)
+
+
 
 
 root.bind('<Button-1>', add_point)
 root.bind('z', stop)
 root.mainloop()
 
-print(point_coordinates)
