c6a63cf4db8e6637bb930f9a5384eb14774d5c1b
Lindsay Barton <lindsaybarton76@gmail.com>
2025-01-25 02:00:18 -0500

    Added plant_info

--- planter.py
+++ planter.py
@@ -1,7 +1,14 @@
 from tkinter import *
+from shapely.geometry import Polygon
+import json
+
+f = open('plant_info.json')
+
+plant_data = json.load(f)
+
 root = Tk()
 
-root.geometry("400x400")
+root.geometry("400x600")
 
 
 drawing_window_frame = Frame(root)
@@ -16,18 +23,21 @@ point_coordinates = []
 ##this window will always be the last coordinate drawn, and the user's next one
 point_coordinates_window = []
 
+##list of coordinates for each individual shape
+areas = []
+
 def add_point(event):
 
     x, y = event.x, event.y
 
     label = Label(drawing_window_frame, text=f"{x} : {y}")
-    label.pack(pady=40)
+    ##label.pack()
 
     point_coordinates.append((x, y))
     point_coordinates_window.append((x, y))
     if(len(point_coordinates_window) > 1):
-        draw_connected_line()
         label.destroy()
+        draw_connected_line()
 
 
 
@@ -40,19 +50,28 @@ def draw_connected_line():
 
 ##stop drawing, connect to first coordinate
 def stop(event):
-
-    print(point_coordinates_window)
+    drawing_window.create_line((point_coordinates[0], point_coordinates_window[0]), fill='black', width=5)
+    
     point_coordinates_window.pop()
-    drawing_window.create_line((point_coordinates[0], (0, 0)), fill='black', width=5)
+
+    ##create polygon from the points and get it's area -- add it to the areas list
+    areas.append(Polygon(point_coordinates).area)
+
+    point_coordinates.clear()
 
 ##updates mouse label with distance between two points
 def update_mouse_label():
     drawing_window.after(500, update_mouse_label)
 
+def create_plant(plant_name):
+    width = plant_data['plants'][plant_name]['minimum_space_inches']
+    drawing_window.create_rectangle(0, 0, width, width, outline = "black", fill = "green", width = 2)
 
 
 
 root.bind('<Button-1>', add_point)
 root.bind('z', stop)
 root.mainloop()
+
+print(areas)
 
